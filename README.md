244a70feda4ba6f9bd53c8724b46c23b8be1cfd7
Piasy <xz4215@gmail.com>
2016-06-28 15:58:13 +0800

    first runnable version

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SqlDelightBin
+
 A runnable jar that uses SqlDelight-Compiler to compile SQL into Java.
+
+## Usage
+
+1. `cd` into the module root directory, generally is the parent directory of `src`;
+2. put your `*.sq` inside `src/main/sqldelight` (it's stronly recommended that you
+use the official SqlDelight gradle plugin to debug your SQL statements first);
+3. run `java -jar <path to SqlDelightBin jar> src/main/sqldelight`, the generated
+Java file will be in `build/generated/source/sqldelight/` dir;
---
+++ src/main/java/com/github/piasy/sqldelight/bin/SqlDelightRunner.java
@@ -0,0 +1,68 @@
+package com.github.piasy.sqldelight.bin;
+
+import com.squareup.sqldelight.SqliteCompiler;
+import com.squareup.sqldelight.SqliteLexer;
+import com.squareup.sqldelight.SqliteParser;
+import com.squareup.sqldelight.Status;
+import com.squareup.sqldelight.model.FileKt;
+import com.squareup.sqldelight.types.SymbolTable;
+import com.squareup.sqldelight.validation.QueryResults;
+import com.squareup.sqldelight.validation.SqlDelightValidator;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+/**
+ * Created by piasy on 6/27/16.
+ */
+public class SqlDelightRunner {
+  public static void main(String[] argv) {
+    File inputDir = new File(argv[0]);
+    for (File sq : listAllFiles(inputDir)) {
+      System.out.println("compiling " + sq.getAbsolutePath() + " ...");
+      try {
+        SqliteLexer lexer = new SqliteLexer(new ANTLRInputStream(new FileInputStream(sq)));
+        SqliteParser parser = new SqliteParser(new CommonTokenStream(lexer));
+        SqliteParser.ParseContext parseContext = parser.parse();
+
+        SymbolTable symbolTable = new SymbolTable();
+        String relativePath = FileKt.relativePath(sq.getAbsolutePath(), parseContext);
+        symbolTable = symbolTable.plus(
+            new SymbolTable(parseContext, sq.getName(), relativePath, Collections.emptyList()));
+        SqlDelightValidator validator = new SqlDelightValidator();
+        Status.ValidationStatus status = validator.validate(relativePath, parseContext, symbolTable);
+        if (status instanceof Status.ValidationStatus.Invalid) {
+          ((Status.ValidationStatus.Invalid) status).getErrors()
+              .forEach(error -> System.out.println(
+                  error.getOriginatingElement() + ": " + error.getErrorMessage()));
+          System.exit(1);
+        } else {
+          List<QueryResults> queries = ((Status.ValidationStatus.Validated) status).getQueries();
+          SqliteCompiler.Companion.write(parseContext, queries, relativePath, ".");
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  private static List<File> listAllFiles(File dir) {
+    if (dir.listFiles() == null) {
+      return Collections.emptyList();
+    }
+    List<File> files = new ArrayList<>();
+    for (File file : dir.listFiles()) {
+      if (file.isDirectory()) {
+        files.addAll(listAllFiles(file));
+      } else {
+        files.add(file);
+      }
+    }
+    return files;
+  }
+}
