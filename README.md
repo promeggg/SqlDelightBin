7c5434a7cd05f76098f961987471a6e405d20624
Piasy <xz4215@gmail.com>
2016-07-08 22:10:41 +0800

    update SqlDelight to v0.4.3

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 A runnable jar that uses SqlDelight-Compiler to compile SQL into Java.
 
+[ ![Download](https://api.bintray.com/packages/okbuild/maven/SqlDelightBin/images/download.svg) ](https://bintray.com/okbuild/maven/SqlDelightBin/_latestVersion)
+
 ## Usage
 
 1. put your `*.sq` inside `src/main/sqldelight` (it's stronly recommended that you
 use the official SqlDelight gradle plugin to debug your SQL statements first);
 3. run `java -jar <path to SqlDelightBin jar> <path to src/main/sqldelight> <output dir>`, the generated
-Java file will be in `<output dir>/build/generated/source/sqldelight/` dir;
+Java file will be in `<output dir>` dir;
--- src/main/java/com/github/piasy/sqldelight/bin/SqlDelightRunner.java
+++ src/main/java/com/github/piasy/sqldelight/bin/SqlDelightRunner.java
@@ -1,12 +1,13 @@
 package com.github.piasy.sqldelight.bin;
 
+import com.squareup.javapoet.JavaFile;
 import com.squareup.sqldelight.SqliteCompiler;
 import com.squareup.sqldelight.SqliteLexer;
 import com.squareup.sqldelight.SqliteParser;
 import com.squareup.sqldelight.Status;
 import com.squareup.sqldelight.model.FileKt;
+import com.squareup.sqldelight.resolution.query.QueryResults;
 import com.squareup.sqldelight.types.SymbolTable;
-import com.squareup.sqldelight.validation.QueryResults;
 import com.squareup.sqldelight.validation.SqlDelightValidator;
 import java.io.File;
 import java.io.FileInputStream;
@@ -33,24 +34,57 @@ public class SqlDelightRunner {
         SqliteParser.ParseContext parseContext = parser.parse();
 
         SymbolTable symbolTable = new SymbolTable();
-        String relativePath = FileKt.relativePath(sq.getAbsolutePath(), parseContext);
+        String relativePath =
+            joinToString(FileKt.relativePath(sq.getAbsolutePath(), File.separatorChar),
+                File.separator);
         symbolTable = symbolTable.plus(
             new SymbolTable(parseContext, sq.getName(), relativePath, Collections.emptyList()));
         SqlDelightValidator validator = new SqlDelightValidator();
-        Status.ValidationStatus status = validator.validate(relativePath, parseContext, symbolTable);
-        if (status instanceof Status.ValidationStatus.Invalid) {
-          ((Status.ValidationStatus.Invalid) status).getErrors()
+        Status.ValidationStatus validationStatus =
+            validator.validate(relativePath, parseContext, symbolTable);
+        if (validationStatus instanceof Status.ValidationStatus.Invalid) {
+          ((Status.ValidationStatus.Invalid) validationStatus).getErrors()
               .forEach(error -> System.out.println(
                   error.getOriginatingElement() + ": " + error.getErrorMessage()));
           System.exit(1);
-        } else {
-          List<QueryResults> queries = ((Status.ValidationStatus.Validated) status).getQueries();
-          SqliteCompiler.Companion.write(parseContext, queries, relativePath, output);
+        }
+        List<QueryResults> queries =
+            ((Status.ValidationStatus.Validated) validationStatus).getQueries();
+        Status compileStatus = SqliteCompiler.Companion.compile(parseContext, queries,
+            ((Status.ValidationStatus.Validated) validationStatus).getViews(), relativePath);
+        if (compileStatus instanceof Status.Failure) {
+          System.out.println(compileStatus.getOriginatingElement()
+              + ": " + ((Status.Failure) compileStatus).getErrorMessage());
+          System.exit(1);
+        } else if (compileStatus instanceof Status.Success) {
+          JavaFile.builder(joinToString(
+              dropLast(FileKt.relativePath(sq.getAbsolutePath(), File.separatorChar), 1), "."),
+              ((Status.Success) compileStatus).getModel()).build()
+              .writeTo(new File(output));
         }
       } catch (IOException e) {
         e.printStackTrace();
       }
     }
+  }
+
+  private static String joinToString(List<String> list, String separator) {
+    String result = "";
+    for (int i = 0, size = list.size(); i < size; i++) {
+      result += list.get(i);
+      if (i != size - 1) {
+        result += separator;
+      }
+    }
+    return result;
+  }
+
+  private static <T> List<T> dropLast(List<T> list, int n) {
+    ArrayList<T> dropped = new ArrayList<>(list.size() - n);
+    for (int i = 0, last = list.size() - n; i < last; i++) {
+      dropped.add(list.get(i));
+    }
+    return dropped;
   }
 
   private static List<File> listAllFiles(File dir) {
